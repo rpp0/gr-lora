ba4e72b232583c27020c05e9ee2862a05f58ff8f
Pieter Robyns <pieter.robyns@uhasselt.be>
2017-09-08 14:04:47 +0200

    Fix bug in Docker scripts

--- docker/docker_build_image.sh
+++ docker/docker_build_image.sh
@@ -2,4 +2,4 @@
 
 LATEST_VERSION=`git ls-remote https://github.com/rpp0/gr-lora.git | grep HEAD | cut -f 1`
 docker build -t rpp0/gr-lora --build-arg CACHEBUST=$LATEST_VERSION .
-docker tag rpp0/gr-lora:latest rpp0/gr-lora:$LATEST_VERSION
+#docker tag rpp0/gr-lora:latest rpp0/gr-lora:$LATEST_VERSION
--- docker/docker_run_grlora.sh
+++ docker/docker_run_grlora.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-LATEST_VERSION=`git ls-remote https://github.com/rpp0/gr-lora.git | grep HEAD | cut -f 1`
 DOCKER_XAUTH=/tmp/.docker.xauth
 touch /tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $DOCKER_XAUTH nmerge -
-docker run -i -t --rm --privileged -e DISPLAY=$DISPLAY -e XAUTHORITY=$DOCKER_XAUTH -v /tmp/.docker.xauth:/tmp/.docker.xauth:ro -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v /dev/bus/usb:/dev/bus/usb --entrypoint /bin/bash rpp0/gr-lora:$LATEST_VERSION
+docker run -i -t --rm --privileged -e DISPLAY=$DISPLAY -e XAUTHORITY=$DOCKER_XAUTH -v /tmp/.docker.xauth:/tmp/.docker.xauth:ro -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v /dev/bus/usb:/dev/bus/usb --entrypoint /bin/bash rpp0/gr-lora:latest
