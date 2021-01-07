0101ec36df8c24ae2dd602c8f1d7449733a42922
Weixuan XIAO <veyx.shaw@gmail.com>
2021-01-07 11:46:25 +0100

    Add boost::placeholders namespace to _1
    Placeholders are not exported to global namespace since boost 1.73.
    See [boost/bind.hpp](https://github.com/boostorg/bind/blob/e3cf787dff2684e7bc45bb84dfa390514656b110/include/boost/bind.hpp#L36):
    
    ```cpp
    BOOST_PRAGMA_MESSAGE(
      "The practice of declaring the Bind placeholders (_1, _2, ...) "
      "in the global namespace is deprecated. Please use "
      "<boost/bind/bind.hpp> + using namespace boost::placeholders, "
      "or define BOOST_BIND_GLOBAL_PLACEHOLDERS to retain the current behavior."
    )
    ```
    
    The placeholders are always defined in boost::placeholders namesapce.
    And they were exported to the global namespace before 1.73. So, adding
    the namespace will not affect the compilation with earlier version.

--- lib/message_socket_sink_impl.cc
+++ lib/message_socket_sink_impl.cc
@@ -47,7 +47,7 @@ namespace gr {
             d_layer(layer) {
 
             message_port_register_in(pmt::mp("in"));
-            set_msg_handler(pmt::mp("in"), boost::bind(&message_socket_sink_impl::handle, this, _1));
+            set_msg_handler(pmt::mp("in"), boost::bind(&message_socket_sink_impl::handle, this, boost::placeholders::_1));
 
             d_socket = socket(AF_INET, SOCK_DGRAM, 0);
 
